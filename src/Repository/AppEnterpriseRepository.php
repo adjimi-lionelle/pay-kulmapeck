2fbc48aa6cfcaa194e22863ab397fa354ba85e5b
adjimi-lionelle <lionelleadjimi@gmail.com>
2025-02-20 05:07:33 +0100

    added

--- src/Repository/AppEnterpriseRepository.php
+++ src/Repository/AppEnterpriseRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<AppEntreprise>
+ * @extends ServiceEntityRepository<AppEnterprise>
  */
 class AppEnterpriseRepository extends ServiceEntityRepository
 {
